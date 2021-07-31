f2c42d2c931aeb3f2ab6ad46ff3f793c20eb3f5e
faris <faris38kanbur@gmail.com>
2021-07-31 19:49:32 +0200

    fnish

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,16 +1,142 @@
-import React from 'react'
+import React, {useRef, useEffect, useCallback} from 'react'
+import { useSpring, animated } from 'react-spring'
 import styled from 'styled-components'
+// import { MdClose } from 'react-icons/md'
+import { FaBeer } from 'react-icons/fa';
+import modalpic from './fk.jpg'
+
+const Background = styled.div`
+width: 100%;
+height: 100%;
+background: rgba(0, 0, 0, 0.8);
+position: fixed;
+display: flex;
+justify-content: center;
+align-items: center;
+`
+ 
+const ModalWrapper = styled.div`
+width: 800px;
+height: 500px;
+box-shadow: 0 5px 16px rgba(0, 0, 0, 0.2);
+background: #fff;
+color: #000;
+display: grid;
+grid-template-columns:1fr 1fr;
+position: relative;
+z-index: 10;
+border-radius: 10px;
+`
+
+const ModalImg = styled.img`
+width: 100%;
+height: 100%;
+border-radius: 10px 0 0 10px;
+background: #555;
+`
+
+const ModalContent = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+line-height: 1.8;
+color: #141414;
+
+p {
+    margin-bottom: 1rem;
+}
+
+button {
+    padding: 10px 24px;
+    background: #141414;
+    color: #fff;
+    border: none;
+}
+`
+
+const CloseModalButton = styled(FaBeer)`
+cursor: pointer;
+position: absolute;
+top:20px;
+right:20px;
+width: 32px;
+height: 32px;
+padding: 0;
+z-index: 10;
+
+`
 
 
 
 
 const Modal = ({modal, setmodal}) => {
-    return (
-        <div>
+    const modalRef = useRef();
+    // animasyon add
+    const animation = useSpring({
+        config: {
+            duration: 250
+        },
+        opacity: modal ? 1: 0,
+        transfrom: modal ? `translateY(0%)` : 'translateY(-100%)'
+    })
 
-            {modal ? <h1>Modal</h1> : null}
-        </div>
+    // ekran disina basinca kapana Background da onclik eklendi
+    const closeModal = (e) => {
+        if(modalRef.current === e.target) {
+            setmodal(false)
+        }
+    }
+
+
+    //keyPress eklendi
+    const keyPress = useCallback(e => {
+        if(e.key === 'Escape'&& modal ) {
+            setmodal(false)
+        }
+        })
+
+    useEffect(
+        () => {
+            document.addEventListener('keydown', keyPress);
+            return () => document.removeEventListener('keydown', keyPress)
+        },
+        [keyPress]
+    )    
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return (
+        <>
+       {
+           modal ? (
+            <Background ref={modalRef} onClick={closeModal}>
+                <animated.div style={animation}>
+                <ModalWrapper modal={modal}>
+                    <ModalImg src={modalpic} alt="camera" />
+                    <ModalContent>
+                        <h1>Are You Ready</h1>
+                        <p>Get exclussive acces to our website</p>
+                        <button>Join Now</button>
+                    </ModalContent>
+                    <CloseModalButton aria-label='Close modal' onClick={() => setmodal(modal => !modal)} />
+                </ModalWrapper>
+                </animated.div>
+            </Background>
+           ) : null}
+       
+        </>
     )
-}
+};
 
 export default Modal
